b935c9573fa0d38214f9ccaa1641d78ec694e164
fabiomayk510 <fabiomayk510@gmail.com>
2023-10-10 12:31:54 -0300

    pagina add

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ app.get('/', (req, res) => {
 })
 
 app.get('/adcionar', (req, res) => {
-    res.render("add.ejs")
+    axios.get("http://localhost:7000/Pecas").then(peca => {
+        var pecas = peca.data;
+        res.render("add.ejs", {pecas: pecas})
+    })
 })
 
 app.post('/add', (req, res) => {
